4a7482c047e7161692da0c086e257dc306e25a38
Nikola Yurukov <nyurukov@shutterfly.com>
2016-12-18 16:30:51 +0200

    [Dev] Change to demo table

--- src/main/java/is/petabytes/pestering/model/GoogleCloudConnector.java
+++ src/main/java/is/petabytes/pestering/model/GoogleCloudConnector.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class GoogleCloudConnector {
-	private static final String COMPANY_SENTIMENT_TIMEPOINT_KIND = "CompanySentimentTimePointTest";
+	private static final String COMPANY_SENTIMENT_TIMEPOINT_KIND = "CompanySentimentTimePointDemo";
 	private static final String COMPANY_NAME = "CompanyName";
 	private static final String DATE = "Date";
 	private static final String SENTIMENT = "Sentiment";
